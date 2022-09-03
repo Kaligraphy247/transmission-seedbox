465193539f39539769db961b4f1ef8460e2f8fab
kaligraphy247 <chikeziejames3@gmail.com>
2022-09-03 19:56:18 +0200

    gunicorn config update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,1 +1,10 @@
-GUNICORN_CMD_ARGS="--bind=0.0.0.0 --workers=4 --reload=True --timeout=120 --worker-class gevent" gunicorn wsgi:app
+# GUNICORN_CMD_ARGS="--bind=0.0.0.0 --workers=4 --reload=True --timeout=120 --worker-class gevent" gunicorn wsgi:app
+
+
+bind = "0.0.0.0"
+backlog = 64
+worker_class = "gevent"
+workers = 4
+timeout = 120
+worker_connections = 1000
+keep_alive = 2
