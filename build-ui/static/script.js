8d053fe51b080697af7989a7ac2c02b72c96aca8
kaligraphy247 <chikeziejames3@gmail.com>
2022-09-05 08:13:15 +0200

    updated build-ui

--- build-ui/static/script.js
+++ build-ui/static/script.js
@@ -2,20 +2,20 @@
 
 console.log("JS is present")
 
-mobileView = document.getElementById("mobile");
-desktopView = document.getElementById("normal"); 
-console.log(mobileView,  desktopView);
-mobileView.style.display = 'none';
+// mobileView = document.getElementById("mobile");
+// desktopView = document.getElementById("normal"); 
+// console.log(mobileView,  desktopView);
+// mobileView.style.display = 'none';
 
-function switchToMobileView() {
-    if (mobileView.style.display = 'none') {
-        mobileView.style.display = '';
-    }
-}
+// function switchToMobileView() {
+//     if (mobileView.style.display = 'none') {
+//         mobileView.style.display = '';
+//     }
+// }
 
-function switchToDesktopView() {
-    if (desktopView.style.display != 'none') {
-        mobileView.style.display = 'none';
-    }
-}
+// function switchToDesktopView() {
+//     if (desktopView.style.display != 'none') {
+//         mobileView.style.display = 'none';
+//     }
+// }
 
